cc46488dacbbf82c900b387a679f9e65acf1c01a
Nina <nina.korocevic@icloud.com>
2022-01-25 13:06:35 +0300

    feat: implement brackets matching function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  let currValStack = '';
+  let currChar = '';
+  let pairNum = -1;
+
+  for (let i = 0; i <= str.length - 1; i++) {
+    if (stack.length != 0) {
+      currValStack = stack.pop();
+      currChar = str.charAt(i);
+      for (let bracketsPair of bracketsConfig) {
+        if (currValStack === bracketsPair[0])
+          pairNum = bracketsConfig.indexOf(bracketsPair);
+      }
+      if (!(pairNum >= 0 && currChar === bracketsConfig[pairNum][1])) {
+        stack.push(currValStack);
+        stack.push(currChar);
+      }
+    } else stack.push(str.charAt(i));
+  }
+  return stack.length === 0 ? true : false;
+};
